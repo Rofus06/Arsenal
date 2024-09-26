932ae6613a03b0897db82485c3d2e73458f0ad4a
Rofus06 <Filipsmail@yahoo.com>
2024-09-26 15:43:11 +0200

    first commit

---
+++ Program.cs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Skapa ett nytt vapen
+        Weapon myWeapon = new Weapon("Sword", 5, 10);
+
+        // Testa vapnet genom att göra en attack
+        int damage = myWeapon.Attack();
+        Console.WriteLine($"You attack with {myWeapon.Name} and deal {damage} damage.");
+    }
+}
